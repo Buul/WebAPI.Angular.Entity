8fbe77b17f42ed2b7938d32c2e4aca0ed45d340d
PAULO OTAVIO FIRMINO SILVA <pof_cc@hotmail.com>
2017-03-12 16:00:34 -0300

    Initial commit

---
+++ WebApiAngular/Controllers/ClienteListagemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiAngular.Controllers
+{
+    public class ClienteListagemController : Controller
+    {
+        // GET: ClienteListagem
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApiAngular/Scripts/AngularAPP/clienteController.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', []);
+app.controller('clienteController', function ($scope, $http) {
+    $http({
+        method: 'GET',
+        url: 'http://localhost:40761/api/ClientesWebAPI/Getcliente'
+    }).then(function (success) {
+        $scope.listaclientes = success.data;
+    }, function (error) {
+        $scope.erro = "Erro: Não possível carregar a listagem de clientes.";
+    });
+});
